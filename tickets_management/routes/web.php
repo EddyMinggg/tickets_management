b600b3df394ae9de93a81ad08cf15418c445c843
Ming <q1240064774@gmail.com>
2025-11-19 01:03:58 +0800

    Fix: 修正路由順序和路徑 - 確保購入來源和售出詳情路由正確匹配

--- tickets_management/routes/web.php
+++ tickets_management/routes/web.php
@@ -21,26 +21,26 @@ Route::post('/purchase', [TicketController::class, 'storePurchase'])->name('tick
 Route::get('/batch-purchase', [TicketController::class, 'createBatchPurchase'])->name('tickets.purchase.batch');
 Route::post('/batch-purchase', [TicketController::class, 'storeBatchPurchase'])->name('tickets.store.batch.purchase');
 
-// 賣出流程
+// 購入來源管理 - 必須在 {ticket} 之前
+Route::get('/{ticket}/sources', [TicketSourceController::class, 'create'])->name('tickets.sources.create');
+Route::get('/{ticket}/sources/new', [TicketSourceController::class, 'add'])->name('tickets.sources.add');
+Route::post('/{ticket}/sources', [TicketSourceController::class, 'store'])->name('tickets.sources.store');
+Route::get('/{ticket}/sources/{source}/edit', [TicketSourceController::class, 'edit'])->name('tickets.sources.edit');
+Route::patch('/{ticket}/sources/{source}', [TicketSourceController::class, 'update'])->name('tickets.sources.update');
+Route::delete('/{ticket}/sources/{source}', [TicketSourceController::class, 'destroy'])->name('tickets.sources.destroy');
+
+// 售出詳情管理 - 必須在 {ticket}/sale 之前
+Route::get('/{ticket}/sales', [TicketSaleController::class, 'create'])->name('tickets.sales.create');
+Route::get('/{ticket}/sales/new', [TicketSaleController::class, 'add'])->name('tickets.sales.add');
+Route::post('/{ticket}/sales', [TicketSaleController::class, 'store'])->name('tickets.sales.store');
+Route::get('/{ticket}/sales/{sale}/edit', [TicketSaleController::class, 'edit'])->name('tickets.sales.edit');
+Route::patch('/{ticket}/sales/{sale}', [TicketSaleController::class, 'update'])->name('tickets.sales.update');
+Route::patch('/{ticket}/sales/{sale}/status', [TicketSaleController::class, 'updateStatus'])->name('tickets.sales.update.status');
+Route::delete('/{ticket}/sales/{sale}', [TicketSaleController::class, 'destroy'])->name('tickets.sales.destroy');
+
+// 賣出流程 - 必須在最後
 Route::get('/{ticket}/sale', [TicketController::class, 'createSale'])->name('tickets.sale');
 Route::post('/{ticket}/sale', [TicketController::class, 'storeSale'])->name('tickets.store.sale');
-
-// 購入來源管理
-Route::get('/{ticket}/sources', [TicketSourceController::class, 'create'])->name('tickets.sources.create');
-Route::post('/{ticket}/source', [TicketSourceController::class, 'store'])->name('tickets.sources.store');
-Route::get('/{ticket}/source/new', [TicketSourceController::class, 'add'])->name('tickets.sources.add');
-Route::get('/{ticket}/source/{source}/edit', [TicketSourceController::class, 'edit'])->name('tickets.sources.edit');
-Route::patch('/{ticket}/source/{source}', [TicketSourceController::class, 'update'])->name('tickets.sources.update');
-Route::delete('/{ticket}/source/{source}', [TicketSourceController::class, 'destroy'])->name('tickets.sources.destroy');
-
-// 售出詳情管理
-Route::get('/{ticket}/sales', [TicketSaleController::class, 'create'])->name('tickets.sales.create');
-Route::post('/{ticket}/sale-detail', [TicketSaleController::class, 'store'])->name('tickets.sales.store');
-Route::get('/{ticket}/sale-detail/new', [TicketSaleController::class, 'add'])->name('tickets.sales.add');
-Route::get('/{ticket}/sale-detail/{sale}/edit', [TicketSaleController::class, 'edit'])->name('tickets.sales.edit');
-Route::patch('/{ticket}/sale-detail/{sale}', [TicketSaleController::class, 'update'])->name('tickets.sales.update');
-Route::patch('/{ticket}/sale-detail/{sale}/status', [TicketSaleController::class, 'updateStatus'])->name('tickets.sales.update.status');
-Route::delete('/{ticket}/sale-detail/{sale}', [TicketSaleController::class, 'destroy'])->name('tickets.sales.destroy');
 
 // 刪除
 Route::delete('/{ticket}', [TicketController::class, 'destroy'])->name('tickets.destroy');
